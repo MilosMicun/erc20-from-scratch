4c729dad33143395cb5e10dccc64e7e48afb0756
Milos Mirkovic <milosdmirkovic@gmail.com>
2026-02-27 13:05:38 +0100

    feat: ERC20 skeleton (transfer/approve/transferFrom)

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -12,4 +12,49 @@ contract MyToken{
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ){
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = 1_000_000 * 10 ** uint256(_decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool){
+        uint256 fromBal = balanceOf[msg.sender];
+        require(fromBal >= amount, "INSUFFICIENT_BAL");
+        require(to != address(0), "ZERO_ADDR");
+        balanceOf[msg.sender] = fromBal - amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool){
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+        
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool){
+        require(to != address(0), "ZERO_ADDR");
+        uint256 allowed = allowance[from][msg.sender];
+        require(allowed >= amount, "NOT_ALLOWED");
+        uint256 fromBal = balanceOf[from];
+        require(fromBal >= amount, "INSUFFICIENT_BAL");
+        allowance[from][msg.sender] = allowed - amount;
+        balanceOf[from] = fromBal - amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+        emit Approval(from, msg.sender, allowance[from][msg.sender]);
+        return true; 
+    }
+
 }
